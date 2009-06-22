c84b4c444cafcce082c68657931e740f87ead652
Adrian Duyzer <adrian@Macbook-Pro.local>
2009-06-22 13:24:38 -0400

    working on getting a directory list

--- o.rb
+++ o.rb
@@ -3,7 +3,18 @@ require 'sinatra' unless defined? ::Sinatra
 
 require 'ruby-debug'
 require 'Haml'
+require 'pathname'
 
 get '/' do
+  @galleries = get_galleries
   haml :index
+end
+
+def get_galleries
+  galleries = []
+  p = Pathname.new("./public/photos")
+  p.each_entry do |entry|
+    galleries << entry.dirname if entry.directory?
+  end
+  galleries
 end
